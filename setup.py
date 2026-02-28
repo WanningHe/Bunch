0f461ca6b52d4eb112ee880f256de9b8defa2981
Wanning He <wanning.he@gmail.com>
2026-02-28 10:53:51 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Bunch',
+    version='1.0.0',
+    description="Helpful tools for manipulating bunches of bananas.",
+    packages = find_packages(),
+    author="Wanning He",
+    author_email="wanning.he@gmail.com"
+)

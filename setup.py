7e26f86e65982aad192b12ec45f21e8e4fc44812
WanningHe <85801861+WanningHe@users.noreply.github.com>
2026-02-28 10:57:44 -0500

    bananas

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='Bunch',
     version='1.0.0',
-    description="Helpful tools for manipulating bunches of bananas.",
+    description="tools for manipulating bananas.",
     packages = find_packages(),
     author="Wanning He",
     author_email="wanning.he@gmail.com"
